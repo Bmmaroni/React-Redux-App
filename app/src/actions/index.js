794a185ec1fc51f79c8269b453f34d2138004e44
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-05 19:27:50 -0700

    finished building out GoatGallery

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -6,10 +6,10 @@ export const getGoats = () => (dispatch) => {
     .get ('http://placegoat.com/200')
     .then(res => {
       console.log('Response: ', res);
-      dispatch({type: 'FETCHING_GOATS_SUCCESS'});
+      dispatch({type: 'FETCHING_GOATS_SUCCESS', payload: res.data});
     })
     .catch(err => {
       console.log('Error: ', err);
-      dispatch({type: 'FETCHING_GOATS_FAILURE'});
+      dispatch({type: 'FETCHING_GOATS_FAILURE', payload: err});
     })
 };
