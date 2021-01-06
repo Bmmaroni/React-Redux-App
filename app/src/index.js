095577b8eef97d2579a1a3b361f7ec584b0c24bb
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-05 17:58:24 -0700

    start of project

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <GoatGallery />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
